779b16c0114f456ecdc2615ad68dde7516248f28
Shivanshu pal <palshivanshu2005@gmail.com>
2026-07-31 03:02:18 +0530

    Update Socket.IO CORS configuration to include production frontend

--- Backend/server.js
+++ Backend/server.js
@@ -160,9 +160,9 @@ ConnectDB().then(() => {
   // Configure Socket.io
   const io = new Server(server, {
       cors: {
-          origin: process.env.ALLOWED_ORIGINS 
-              ? process.env.ALLOWED_ORIGINS.split(',') 
-              : ['http://localhost:5173', 'http://localhost:5174', 'http://localhost:5175'],
+          origin: process.env.ALLOWED_ORIGINS
+              ? process.env.ALLOWED_ORIGINS.split(',')
+              : ['http://localhost:5173', 'http://localhost:5174', 'http://localhost:5175', 'https://main.d1s3vvkwvl4zr3.amplifyapp.com'],
           credentials: true
       }
   })
